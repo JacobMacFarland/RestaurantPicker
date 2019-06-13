b9c7ac9c68ec77a3deb6aa67aadd879ede848761
Jacob MacFarland <macfarlandjw@appstate.edu>
2019-06-12 20:04:20 -0500

    Initial Commit

---
+++ RestaurantPicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RestaurantPicker
+//
+//  Created by Jacob MacFarland on 6/12/19.
+//  Copyright © 2019 Jacob MacFarland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
